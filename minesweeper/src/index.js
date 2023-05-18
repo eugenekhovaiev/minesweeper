f568f7d3cf8e819893720c2946ee908a8990af3e
Yevhenii Khovaiev <ekhovaev@gmail.com>
2023-05-18 03:28:33 +0300

    refactor: move game options and matrix-based functions to separate module

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,125 +1,20 @@
 import './sass/main.scss';
 
 import { createField, createContainer } from './modules/editHTML';
+import {
+  createMatrix,
+  openCell,
+  toggleFlag,
+  openSurrCells,
+} from './modules/matrix';
 
-const WIDTH = 10;
-const HEIGHT = 10;
-const BOMBS_AMOUNT = 10;
+import {
+  HEIGHT,
+  WIDTH,
+  BOMBS_AMOUNT,
+} from './modules/game-options';
 
 function minesweeperStart(fieldWidth, fieldHeight, fieldBombsAmount) {
-  function getIndex(row, col) {
-    return row * fieldWidth + col;
-  }
-  const getRow = (index) => Math.floor(index / fieldWidth);
-  const getCol = (index) => index % fieldWidth;
-
-  function isValid(row, col) {
-    return row >= 0 && row < fieldHeight && col >= 0 && col < fieldWidth;
-  }
-
-  function getSurrCells(index, matrix) {
-    const surrCells = [];
-
-    const row = getRow(index);
-    const col = getCol(index);
-
-    for (let i = -1; i <= 1; i += 1) {
-      for (let j = -1; j <= 1; j += 1) {
-        if (isValid(row + i, col + j)) {
-          surrCells.push(matrix[getIndex(row + i, col + j)]);
-        }
-      }
-    }
-
-    return surrCells;
-  }
-
-  function findButtonIndex(button, matrix) {
-    return matrix.findIndex((cell) => cell.button === button);
-  }
-
-  function createMatrix(width, height, buttonsArray) {
-    const matrix = [];
-    for (let i = 0; i < height; i += 1) {
-      for (let j = 0; j < width; j += 1) {
-        const cell = {
-          row: i,
-          col: j,
-          button: buttonsArray[getIndex(i, j)],
-        };
-
-        matrix.push(cell);
-      }
-    }
-    return matrix;
-  }
-
-  function addBombs(matrix, bombsAmount, safeButton) {
-    const newMatrix = [...matrix];
-
-    const safeButtonIndex = findButtonIndex(safeButton, newMatrix);
-    const unminedCells = getSurrCells(safeButtonIndex, matrix);
-
-    let minedCells = [...matrix].filter((cell) => !unminedCells.includes(cell));
-    minedCells = minedCells.sort(() => Math.random() - 0.5).slice(0, bombsAmount);
-
-    minedCells.forEach((cell) => {
-      const minedButtonIndex = findButtonIndex(cell.button, matrix);
-      newMatrix[minedButtonIndex].inner = 'bomb';
-    });
-    return newMatrix;
-  }
-
-  function countBombs(index, matrix) {
-    const bombCells = getSurrCells(index, matrix).filter((cell) => cell.inner === 'bomb');
-    return bombCells.length;
-  }
-
-  function addInners(matrix) {
-    const newMatrix = [...matrix];
-    newMatrix.forEach((cell, index) => {
-      if (cell.inner !== 'bomb') {
-        cell.inner = countBombs(index, matrix);
-      }
-    });
-    return newMatrix;
-  }
-
-  function openCell(button, matrix) {
-    const buttonIndex = findButtonIndex(button, matrix);
-    const buttonCell = matrix[buttonIndex];
-
-    if (buttonCell.status === 'opened' || buttonCell.status === 'flag') return true;
-
-    buttonCell.status = 'opened';
-    button.classList.add('cell_opened');
-
-    if (buttonCell.inner === 0) {
-      button.innerHTML = '';
-
-      const surrCells = getSurrCells(buttonIndex, matrix);
-      surrCells.forEach((cell) => {
-        openCell(cell.button, matrix);
-      });
-      return true;
-    }
-
-    if (buttonCell.inner === 'bomb') {
-      button.innerHTML = 'X';
-      return false;
-    }
-
-    button.innerHTML = matrix[buttonIndex].inner;
-    return true;
-  }
-
-  function openAllCells(matrix) {
-    matrix.forEach((cell) => {
-      if (!cell.inner) return;
-      cell.button.innerHTML = cell.inner;
-    });
-  }
-
   function winFunc(event) {
     console.log(event);
     console.log('win');
@@ -128,28 +23,6 @@ function minesweeperStart(fieldWidth, fieldHeight, fieldBombsAmount) {
   function lossFunc(event) {
     console.log(event);
     console.log('loss');
-  }
-
-  function toggleFlag(button, matrix) {
-    const buttonIndex = findButtonIndex(button, matrix);
-    const cell = matrix[buttonIndex];
-    if (cell.status === 'opened') return;
-
-    if (cell.status !== 'flag') {
-      cell.status = 'flag';
-      button.innerHTML = 'F';
-    } else {
-      cell.status = null;
-      button.innerHTML = '';
-    }
-  }
-
-  function openSurrCells(button, matrix) {
-    const buttonIndex = findButtonIndex(button, matrix);
-    const surrCells = getSurrCells(buttonIndex, matrix);
-    surrCells.forEach((cell) => {
-      openCell(cell.button, matrix);
-    });
   }
 
   const container = createContainer(document.body);
@@ -162,9 +35,7 @@ function minesweeperStart(fieldWidth, fieldHeight, fieldBombsAmount) {
     const button = event.target.closest('.cell');
     if (!button) return;
 
-    matrix = createMatrix(fieldWidth, fieldHeight, buttonsArray);
-    matrix = addBombs(matrix, fieldBombsAmount, button);
-    matrix = addInners(matrix);
+    matrix = createMatrix(fieldWidth, fieldHeight, buttonsArray, button);
     // openAllCells(matrix);
     console.log(matrix);
   }, { once: true });
@@ -182,7 +53,7 @@ function minesweeperStart(fieldWidth, fieldHeight, fieldBombsAmount) {
 
     const openedButtonsAmount = field.querySelectorAll('.cell_opened').length;
     const buttonsAmount = fieldWidth * fieldHeight;
-    // TODO: fix winning codition
+    // TODO fix winning codition
     if (openedButtonsAmount + fieldBombsAmount === buttonsAmount && button.innerHTML !== 'X') {
       const winEvent = new Event('win', { bubbles: true });
       button.dispatchEvent(winEvent);
@@ -193,7 +64,7 @@ function minesweeperStart(fieldWidth, fieldHeight, fieldBombsAmount) {
     event.preventDefault();
     const button = event.target.closest('.cell');
     if (!button) return;
-
+    // TODO fix disapering flag
     toggleFlag(button, matrix);
   });
 
