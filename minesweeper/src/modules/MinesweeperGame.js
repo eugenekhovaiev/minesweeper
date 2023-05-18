83ed31f2b7d4429fdcaa67ee3e2d57c012a5f7db
Yevhenii Khovaiev <ekhovaev@gmail.com>
2023-05-18 18:51:14 +0300

    refactor: move MinesweeperGame to separate module

---
+++ minesweeper/src/modules/MinesweeperGame.js
@@ -0,0 +1,87 @@
+import { createField } from './editHTML';
+
+import {
+  createMatrix,
+  openCell,
+  toggleFlag,
+  openSurrCells,
+} from './matrix';
+
+export default class MinesweeperGame {
+  constructor(where) {
+    this.where = where;
+    this.matrix = null;
+    this.field = null;
+  }
+
+  start(fieldWidth, fieldHeight, fieldBombsAmount) {
+    function winFunc(event) {
+      console.log(event);
+      console.log('win');
+    }
+
+    function lossFunc(event) {
+      console.log(event);
+      console.log('loss');
+    }
+
+    this.field = createField(this.where, fieldWidth, fieldHeight);
+    const buttonsArray = Array.from(this.field.querySelectorAll('.cell'));
+
+    this.field.addEventListener('click', (event) => {
+      const button = event.target.closest('.cell');
+      if (!button || this.matrix) return;
+
+      this.matrix = createMatrix(fieldWidth, fieldHeight, buttonsArray, button);
+      // openAllCells(matrix);
+      console.log(this.matrix);
+    });
+
+    this.field.addEventListener('click', (event) => {
+      const button = event.target.closest('.cell');
+      if (!button) return;
+
+      const isSafe = openCell(button, this.matrix);
+
+      if (!isSafe) {
+        const boomEvent = new Event('boom', { bubbles: true });
+        button.dispatchEvent(boomEvent);
+      }
+
+      const openedButtonsAmount = this.field.querySelectorAll('.cell_opened').length;
+      const buttonsAmount = fieldWidth * fieldHeight;
+      // TODO fix winning codition
+      if (openedButtonsAmount + fieldBombsAmount === buttonsAmount && button.innerHTML !== 'X') {
+        const winEvent = new Event('win', { bubbles: true });
+        button.dispatchEvent(winEvent);
+      }
+    });
+
+    this.field.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+      const button = event.target.closest('.cell');
+      if (!button) return;
+      // TODO fix disapering flag
+      toggleFlag(button, this.matrix);
+    });
+
+    this.field.addEventListener('dblclick', (event) => {
+      const button = event.target.closest('.cell');
+      if (!button) return;
+
+      openSurrCells(button, this.matrix);
+    });
+
+    document.addEventListener('boom', lossFunc);
+
+    document.addEventListener('win', winFunc);
+  }
+
+  restart(fieldWidth, fieldHeight, fieldBombsAmount) {
+    this.matrix = null;
+    this.field.remove();
+    this.field = null;
+
+    this.start(fieldWidth, fieldHeight, fieldBombsAmount, this.where);
+  }
+}
