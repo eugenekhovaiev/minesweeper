791eab24c86f9a115cb7aa57c187c5554cff18f5
Yevhenii Khovaiev <ekhovaev@gmail.com>
2023-05-20 03:12:52 +0300

    refactor: remove useless code from Timer

--- minesweeper/src/modules/Timer.js
+++ minesweeper/src/modules/Timer.js
@@ -8,19 +8,13 @@ export default class Timer {
   }
 
   start() {
-    // this.startTime = new Date();
     this.timerId = setInterval(() => {
-      // const currentTime = new Date();
-      // const secondsPassed = Math.floor((currentTime - this.startTime) / 1000);
-      // this.node.innerHTML = secondsPassed;
-      // console.log(secondsPassed);
       this.node.innerHTML = +this.node.innerHTML + 1;
     }, 1000);
   }
 
   stop() {
     clearInterval(this.timerId);
-    console.log('stop');
   }
 
   load(newStart) {
